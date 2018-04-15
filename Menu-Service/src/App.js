fec66df4e0ba65495f40f72a68532a6b30c55917
ACODE91 <aaron.z.lao@gmail.com>
2018-04-14 21:15:43 -0700

    created menu category buttons

--- Menu-Service/src/App.js
+++ Menu-Service/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MenuBox from './MenuBox.js'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      display: false
+    }
+  }
+
+  handleClick() {
+    if(!this.state.display) {
+      this.setState({display: true});
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      <button onClick={this.handleClick.bind(this)}>Display Menu</button>
+      {(this.state.display) ? <MenuBox /> : <div></div>}
       </div>
     );
   }
---
+++ Menu-Service/src/MenuBox.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MenuBox(props) {
+  return (
+    <div className="menuContainer">
+      <div className="appetizers categoryButton">Appetizers</div>
+      <div className="mains categoryButton">Mains</div>
+      <div className="beverages categoryButton">Beverages</div>
+    </div>
+  )
+}
+
+export default MenuBox;
